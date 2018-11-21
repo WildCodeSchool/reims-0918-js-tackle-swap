86c6612c523b9a0fb9ef7a40008c1dd8ed93c15f
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-11-21 11:39:35 +0100

    add path items, item/id, and home

--- back/index.js
+++ back/index.js
@@ -7,11 +7,31 @@ app.get("/", (req, res) => {
 });
 
 app.get("/items", (req, res) => {
-  const limit = req.query.limit ? req.query.limit : 20;
+  const limit =
+    req.query.limit && req.query.limit >= 20
+      ? req.query.limit <= 100
+        ? req.query.limit
+        : 100
+      : 20;
+
   const offset = req.query.offset ? req.query.offset : 0;
+
   res.send(
     `Affichage des produits par ${limit} à partir du produit numéro: ${offset}`
   );
+});
+
+app.get("/item/:id", (req, res) => {
+  const isExist = req.params.id === "4";
+  isExist
+    ? res
+        .status(200)
+        .send(
+          `Affichage de la fiche produit d'un article en fonction de son id : ${
+            req.params.id
+          }`
+        )
+    : res.status(404).send("L'article demandé n'existe pas");
 });
 
 app.listen(port, err => {
