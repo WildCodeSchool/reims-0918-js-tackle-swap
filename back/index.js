e18d7b28f736ad552decb9613c6240a16e02579c
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-11-21 10:32:54 +0100

    create path for items displayed default and switch package to back directory

--- back/index.js
+++ back/index.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.send("Bienvenue le Marketplace incontournable des pêcheurs");
 });
 
+app.get("/items", (req, res) => {
+  const limit = req.query.limit ? req.query.limit : 10;
+  const offset = req.query.offset ? req.query.offset : 0;
+  res.send(
+    `Affichage des produits par ${limit} à partir du produit numéro: ${offset}`
+  );
+});
+
 app.listen(port, err => {
   if (err) {
     throw new Error("Something bad happened...");
