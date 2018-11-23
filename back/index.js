cd7fa72e1e3f8ea23940ad1278b19baacac41c64
Simonin Gautier <gautier08000@hotmail.com>
2018-11-23 10:34:19 +0100

    install .env and doing POST/PUT

--- back/index.js
+++ back/index.js
@@ -1,6 +1,48 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const port = 3000;
+const connection = require("./conf");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.post("/items", (req, res) => {
+  const formData = req.body;
+  connection.query("INSERT INTO item SET ?", formData, (err, results) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(409)
+        .send("La requête ne peut pas être traitée à l'état actuel");
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+app.put("/items/:id", (req, res) => {
+  const idUser = req.params.id;
+  const formData = req.body;
+  connection.query(
+    "UPDATE item SET ? WHERE id = ?",
+    [formData, idUser],
+    err => {
+      if (err) {
+        console.log(err);
+        res
+          .status(409)
+          .send("La requête ne peut pas être traitée à l'état actuel");
+      } else {
+        res.sendStatus(200);
+      }
+    });
+});
 
 app.get("/", (req, res) => {
   res.send("Bienvenue le Marketplace incontournable des pêcheurs");
