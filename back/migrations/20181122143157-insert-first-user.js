7563b78f1d8940e59547b1aa4bc030d54695ac8d
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-11-23 10:22:19 +0100

    install db-migrate & dotenv

---
+++ back/migrations/20181122143157-insert-first-user.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+  db.insert(
+    "users",
+    [
+      "lastname",
+      "firstname",
+      "email",
+      "email_confirmation",
+      "password",
+      "profil_picture",
+      "adress",
+      "city",
+      "postal_code",
+      "gender",
+      "description"
+    ],
+    [
+      "THOMAS",
+      "Mathieu",
+      "kawacke@gmail.com",
+      "zoiueznhliuef75638294775",
+      "password",
+      "NULL",
+      "NULL",
+      "NULL",
+      "08330",
+      "M",
+      "NULL"
+    ],
+    callback
+  );
+};
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  version: 1
+};
