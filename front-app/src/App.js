508843649a8be1bea82e0a805e412fd399c8ee96
LeuthsoulineChanthathirath <r.leuthsouline@gmail.com>
2018-12-05 16:59:30 +0100

    styleList

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router";
+import background from "./images/background-white.jpg";
 
 import ListArticlesContainer from "./containers/ListArticlesContainer";
 import ArticleDetails from "./components/ArticleDetails";
 
 class App extends Component {
   render() {
+    let styles = {
+      backgroundImage: "url(" + background + ")",
+      backgroundSize: "cover",
+      overflow: "hidden"
+    };
     return (
-      <div>
+      <div style={styles}>
         <Switch>
           <Route exact path="/" component={ListArticlesContainer} />
           <Route exact path="/article" component={ArticleDetails} />
