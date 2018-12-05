1d94bb62b86eef6a5f4ed43923c00a33c53d655c
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-12-05 17:52:25 +0100

    add NavBar structure

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -4,11 +4,13 @@ import { Route, Switch } from "react-router";
 
 import ListArticlesContainer from "./containers/ListArticlesContainer";
 import ArticleDetails from "./components/ArticleDetails";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
         <Switch>
           <Route exact path="/" component={ListArticlesContainer} />
           <Route exact path="/article" component={ArticleDetails} />
