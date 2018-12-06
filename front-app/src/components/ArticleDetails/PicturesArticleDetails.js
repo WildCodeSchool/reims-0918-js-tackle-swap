6919c2f1f6710931771bab0310b3655610c07645
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-12-06 15:10:40 +0100

    add carousel image

--- front-app/src/components/ArticleDetails/PicturesArticleDetails.js
+++ front-app/src/components/ArticleDetails/PicturesArticleDetails.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Carousel } from "react-materialize";
 
 const PicturesArticleDetails = () => (
   <div>
-    <p>Images</p>
+    <Carousel
+      images={[
+        "https://lorempixel.com/250/250/nature/1",
+        "https://lorempixel.com/250/250/nature/2",
+        "https://lorempixel.com/250/250/nature/3",
+        "https://lorempixel.com/250/250/nature/4",
+        "https://lorempixel.com/250/250/nature/5"
+      ]}
+    />
   </div>
 );
 
