b4515b061206e2646eba55733c0b45b5d72e9063
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-12-06 15:18:13 +0100

    merged

--- front-app/src/components/ArticleDetails/PicturesArticleDetails.js
+++ front-app/src/components/ArticleDetails/PicturesArticleDetails.js
@@ -5,7 +5,7 @@ const PicturesArticleDetails = ({ picture }) => (
   <div>
     <Carousel
       images={[
-        { picture },
+        "https://lorempixel.com/250/250/nature/1",
         "https://lorempixel.com/250/250/nature/2",
         "https://lorempixel.com/250/250/nature/3",
         "https://lorempixel.com/250/250/nature/4",
