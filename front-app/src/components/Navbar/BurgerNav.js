c8f148ba649da7959bbacb3675dc16447248a9e5
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-12-11 14:54:59 +0100

    remove comments

--- front-app/src/components/Navbar/BurgerNav.js
+++ front-app/src/components/Navbar/BurgerNav.js
@@ -5,12 +5,7 @@ import { withStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
-// import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
-// import ListItemIcon from "@material-ui/core/ListItemIcon";
-// import ListItemText from "@material-ui/core/ListItemText";
-// import InboxIcon from "@material-ui/icons/MoveToInbox";
-// import MailIcon from "@material-ui/icons/Mail";
 import MenuIcon from "@material-ui/icons/Menu";
 
 const styles = {
@@ -48,53 +43,12 @@ class BurgerNav extends React.Component {
             { name: "Paramètres", path: "/" }
           ].map((link, index) => (
             <ListItem button key={index}>
-              {/* <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon> */}
-              {/* <ListItemText primary={link.name} /> */}
               <Link to={link.path}>{link.name}</Link>
             </ListItem>
           ))}
         </List>
-        {/* <Divider />
-        <List>
-          {["Paramètres", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List> */}
       </div>
     );
-
-    // const fullList = (
-    //   <div className={classes.fullList}>
-    //     <List>
-    //       {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-    //         <ListItem button key={text}>
-    //           <ListItemIcon>
-    //             {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-    //           </ListItemIcon>
-    //           <ListItemText primary={text} />
-    //         </ListItem>
-    //       ))}
-    //     </List>
-    //     <Divider />
-    //     <List>
-    //       {["All mail", "Trash", "Spam"].map((text, index) => (
-    //         <ListItem button key={text}>
-    //           <ListItemIcon>
-    //             {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-    //           </ListItemIcon>
-    //           <ListItemText primary={text} />
-    //         </ListItem>
-    //       ))}
-    //     </List>
-    //   </div>
-    // );
 
     return (
       <div>
