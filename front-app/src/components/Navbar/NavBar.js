ea9daed3fb63b739574bfcbe7600e102eb972892
Mathieu THOMAS <kawacke@gmail.com>
2018-12-12 10:28:47 +0100

    Button dont used

--- front-app/src/components/Navbar/NavBar.js
+++ front-app/src/components/Navbar/NavBar.js
@@ -8,7 +8,6 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 
 import Drawer from "@material-ui/core/Drawer";
-import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import MenuIcon from "@material-ui/icons/Menu";
