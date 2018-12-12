dac235239e73c1dc955bef39b8e7e05052db53d6
Mathieu THOMAS <kawacke@gmail.com>
2018-12-12 11:33:20 +0100

    add token in local storage

--- front-app/src/containers/LoginContainer.js
+++ front-app/src/containers/LoginContainer.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import Login from "../components/Login";
+import axios from "axios";
+import ls from "local-storage";
 
 class LoginContainer extends Component {
-  submit = values => console.log(values);
+  submit = values =>
+    axios
+      .post("http://localhost:5000/auth/login", values)
+      .then(result => ls.set("token-tackle-swap", result.data.token));
   render() {
     return <Login onSubmit={this.submit} />;
   }
