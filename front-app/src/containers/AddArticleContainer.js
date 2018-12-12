6ecb945a07b0ac94f132620e882bf50d4cd9f13d
Mathieu THOMAS <kawacke@gmail.com>
2018-12-12 11:49:32 +0100

    Add headers in axios Post to add article

--- front-app/src/containers/AddArticleContainer.js
+++ front-app/src/containers/AddArticleContainer.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import AddArticle from "../components/AddArticle";
 import axios from "axios";
+import ls from "local-storage";
 
 class AddArticleContainer extends Component {
   submit = values =>
-    axios.post(
-      "http://localhost:5000/article",
-      Object.assign({ owner_id: 1 }, values)
-    );
+    axios.post("http://localhost:5000/article", {
+      headers: {
+        accept: "application/json",
+        "content-type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${ls.get("jwt-tackle-swap")}`
+      },
+      body: values
+    });
 
   render() {
     return <AddArticle onSubmit={this.submit} />;
