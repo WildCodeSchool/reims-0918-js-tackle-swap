98d70f9091b7a07632087606ab2e42788d3c6384
Mathieu THOMAS <kawacke@gmail.com>
2019-01-11 11:25:09 +0100

    loink menu

--- front-app/src/components/Navbar/NavBar.js
+++ front-app/src/components/Navbar/NavBar.js
@@ -123,23 +123,27 @@ class ButtonAppBar extends Component {
           {list
             .sort((a, b) => a.id - b.id)
             .map((link, index) => (
-              <ListItem button key={index}>
-                <ListItemIcon>{link.icon}</ListItemIcon>
-
+              <div key={index}>
                 {link.name === "Se déconnecter" ? (
                   <Link
                     to={link.path}
                     onClick={e => this.disconnect(e)}
                     className={classes.Title}
                   >
-                    {link.name}
+                    <ListItem button>
+                      <ListItemIcon>{link.icon}</ListItemIcon>
+                      {link.name}
+                    </ListItem>
                   </Link>
                 ) : (
                   <Link to={link.path} className={classes.Title}>
-                    {link.name}
+                    <ListItem button>
+                      <ListItemIcon>{link.icon}</ListItemIcon>
+                      {link.name}
+                    </ListItem>
                   </Link>
                 )}
-              </ListItem>
+              </div>
             ))}
         </List>
       </div>
