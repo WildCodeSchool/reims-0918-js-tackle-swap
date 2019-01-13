f27bfa8272d70d8bad7999747ed403b6d17b75e6
Mathieu THOMAS <kawacke@gmail.com>
2019-01-13 10:37:22 +0100

    port server

--- back/index.js
+++ back/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const http = require("http");
-const socketIO = require("socket.io");
 
+const socketIO = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
